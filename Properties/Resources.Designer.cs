b6061db6096faa62153a418bb709e169a8ef95a4
Juan Esteban Gonzalez <juanestebangonzalezsierra0327@gmail.com>
2023-04-29 14:31:34 -0500

    Products View

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -173,6 +173,16 @@ namespace Supermarker_mvp.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap save1 {
+            get {
+                object obj = ResourceManager.GetObject("save1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap search {
             get {
                 object obj = ResourceManager.GetObject("search", resourceCulture);
